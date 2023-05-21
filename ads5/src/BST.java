11c83abe209a75b205a937a222b7702ff7eea20e
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-21 23:33:05 +0600

    1

---
+++ ads5/src/BST.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+
+    // Node class representing a node in the binary search tree
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
