4d68ab1b8f189580eb35c22c49aec0d66f68c1af
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-21 23:39:42 +0600

    1

--- ads5/src/Main.java
+++ ads5/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BST<Integer, String> bst = new BST<>();
+
+        bst.put(5, "Value 5"); // Insert key-value pair (5, "Value 5")
+        bst.put(2, "Value 2"); // Insert key-value pair (2, "Value 2")
+        bst.put(7, "Value 7"); // Insert key-value pair (7, "Value 7")
+        bst.put(1, "Value 1"); // Insert key-value pair (1, "Value 1")
+        bst.put(4, "Value 4"); // Insert key-value pair (4, "Value 4")
+        bst.put(6, "Value 6"); // Insert key-value pair (6, "Value 6")
+        bst.put(9, "Value 9"); // Insert key-value pair (9, "Value 9")
+
+        System.out.println("Value for key 4: " + bst.get(4));
+
+        bst.delete(2);
+
+        System.out.println("Size of the tree: " + bst.size());
+
+        System.out.println("Keys in ascending order:");
+        for (Integer key : bst.iterator()) {
+            System.out.println(key);
+        }
     }
 }
