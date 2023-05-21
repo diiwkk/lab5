142f0692c4069ff32479d95c9dce8125b0106c0f
diiwkk <129244932+diiwkk@users.noreply.github.com>
2023-05-21 23:33:56 +0600

    1

--- ads5/src/BST.java
+++ ads5/src/BST.java
@@ -15,4 +15,22 @@ public class BST<K extends Comparable<K>, V> {
             this.val = val;
         }
     }
-}
+    public void put(K key, V value) {
+        root = put(root, key, value);
+    }
+
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            return new Node(key, val);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.val = val;
+        }
+        return node;
+    }
